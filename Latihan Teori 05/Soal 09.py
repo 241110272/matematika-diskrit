281f70389e721d2e5bb47cd7eee96c732fe72d50
241110272 <julius.einstein@students.mikroskil.ac.id>
2024-10-20 12:20:07 +0000

    Changes 4

--- Latihan Teori 05/Soal 09.py
+++ Latihan Teori 05/Soal 09.py
@@ -4,7 +4,7 @@ def hextobin(nums):
     return [bin(int(num, 16))[2:] for num in nums]
 
 q9 = ["80E", "ABBA", "135AB", "DEFACED"]
-q9 = hextobin(q9)
+q9Res = hextobin(q9)
 print("Exercise 4 :")
 for i in range(len(q9)):
     print(f"{i+1} {q9[i]} in binary = {q9Res[i]}")
